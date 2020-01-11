514d9276aade7462327627174b6c9ed4b612bdec
Virx <ericmveilleux@gmail.com>
2020-01-11 14:51:42 -0500

    Code now builds
    Fixed the imports so the code can now build
    Added LimeLight configuration files:
     - camera.elp180.json
     - camera.microsoft.json
     - Pipeline_Name.vpr
    Added a quick Readme

---
+++ README.md
@@ -0,0 +1,3 @@
+# 2020 Infinite Recharge Skatebot
+This is code for the 2020 Infinite Recharge Skatebot.
+This version of the Skatebot has the LimeLight camera and API.
--- src/main/java/frc/robot/lib/CameraFileReader.java
+++ src/main/java/frc/robot/lib/CameraFileReader.java
@@ -1,17 +1,19 @@
 package frc.robot.lib;
 
-import java.io.File;
-import java.nio.file.Files;
-import java.nio.file.Paths;
-import java.util.ArrayList;
-import java.util.List;
-
-import com.fasterxml.jackson.core.JsonParser;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 import com.google.gson.JsonArray;
 import com.google.gson.JsonElement;
 import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
+
 
 /**
  * Parses Camera Definition JSON File
